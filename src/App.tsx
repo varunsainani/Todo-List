e064b0b55c5f967e088b5b7d226a29073074e51b
varunsainani <varunsainani0990@gmail.com>
2024-07-05 12:57:32 +0500

    set css property and modified app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import Input from './components/Input';
 import { Todo } from './model';
@@ -8,16 +8,27 @@ function App() {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  // Load todos from local storage when the component mounts
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  // Save todos to local storage whenever they change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if(todo){
-      setTodos([...todos, {id: Date.now(), todo, completed: false}]);
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, completed: false }]);
       setTodo("");
     }
-  }
-
-  console.log(todos);
+  };
 
   return (
     <div className='App'>
@@ -28,4 +39,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
